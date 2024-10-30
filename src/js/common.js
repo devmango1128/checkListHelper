74b9fb125c7906de3da4adfcb8692308d49eddd7
imsiyeon <ysy0701@osstem.com>
2024-10-30 13:40:08 +0900

    css/js 수정

--- src/js/common.js
+++ src/js/common.js
@@ -1,3 +1,14 @@
 function goMenu(url) {
     window.location.href = url;
 }
+
+function goBack() {
+
+    const previousPage = document.referrer;
+
+    if (previousPage && previousPage !== "") {
+        window.location.href = previousPage;
+    } else {
+        window.location.href = 'main.html';
+    }
+}
