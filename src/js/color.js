f2d88face9d0fe8c461ee64016ea925f0b6981bf
imsiyeon <ysy0701@osstem.com>
2024-11-01 09:15:52 +0900

    color 변경

--- src/js/color.js
+++ src/js/color.js
@@ -1,67 +1,83 @@
 const codeColor = [{
     code : 'M',
     background : '#6696ff',
-    color : '#fff'
+    color : '#fff',
+    iconColor : '#fff',
 },{
     code : 'W',
     background : '#ff6666',
-    color : '#fff'
+    color : '#fff',
+    iconColor : '#fff',
 },{
     code : '001',
     background : '#ffffff',
-    color : '#4b4b4b'
+    color : '#4b4b4b',
+    iconColor : '#cecece',
 },{
     code : '002',
     background : '#ffffff',
-    color : '#4b4b4b'
+    color : '#4b4b4b',
+    iconColor : '#cecece',
 },{
     code : '003',
     background : '#ffffff',
-    color : '#4b4b4b'
+    color : '#4b4b4b',
+    iconColor : '#cecece',
 },{
     code : '004',
     background : '#ffffff',
-    color : '#4b4b4b'
+    color : '#4b4b4b',
+    iconColor : '#cecece',
 },{
     code : '001001',
     background : '#cee9fd',
-    color : '#419aff'
+    color : '#419aff',
+    iconColor : '#cecece',
 },{
     code : '001002',
     background : '#cee9fd',
-    color : '#419aff'
+    color : '#419aff',
+    iconColor : '#cecece',
 },{
     code : '002001',
     background : '#cee9fd',
-    color : '#419aff'
+    color : '#419aff',
+    iconColor : '#cecece',
 },{
     code : '002002',
     background : '#cee9fd',
-    color : '#419aff'
+    color : '#419aff',
+    iconColor : '#cecece',
 },{
     code : '002003',
     background : '#cee9fd',
-    color : '#419aff'
+    color : '#419aff',
+    iconColor : '#cecece',
 },{
     code : '002004',
     background : '#cee9fd',
-    color : '#419aff'
+    color : '#419aff',
+    iconColor : '#cecece',
 },{
     code : '003001',
     background : '#cee9fd',
-    color : '#419aff'
+    color : '#419aff',
+    iconColor : '#cecece',
 },{
     code : '003002',
     background : '#cee9fd',
-    color : '#419aff'
+    color : '#419aff',
+    iconColor : '#cecece',
 },{
     code : '004001',
     background : '#cee9fd',
-    color : '#419aff'
+    color : '#419aff',
+    iconColor : '#cecece',
 },{
     code : '004002',
     background : '#cee9fd',
-    color : '#419aff'
+    color : '#419aff',
+    iconColor : '#cecece',
 }];
 
 function getBackgroundColor(code) {
@@ -72,4 +88,9 @@ function getBackgroundColor(code) {
 function getFontColor(code) {
     const item = codeColor.find(item => item.code === code);
     return item ? item.color : '#fff';
+}
+
+function getBorderColor(code) {
+    const item = codeColor.find(item => item.code === code);
+    return item ? item.iconColor : '#fff';
 }
